2f5d0abafe49bb6ee2e9a30bac20b1f47cb826b6
Nyiobyiringiro <114920983+Nyiobyiringiro@users.noreply.github.com>
2023-02-01 11:47:57 +0200

    in-class task 1

--- layout/ex1.php
+++ layout/ex1.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My first php code</title>
+      
 </head>
 <body>
     <h3>Write a simple PHP script to print your information</h3>
@@ -14,14 +15,42 @@
   ?> 
   <h3>Write PHP code to display the following message</h3>
   <?php 
-   echo"Hello World! My name is \"Aisha\"";
+   echo"Hello World! My name is \"David\"";
    ?>
    <h3>Write the PHP code in to display the current date</h3>
    <?php
    echo"Date: ";
    echo date("d.m.Y")
    ?>
-   
-
+   <h3>variable title</h3>
+   <?php
+   $title="PHP is interesting"
+   ?>
+   <h1><?php echo $title?></h1>
+   <?php
+   $g1=5; 
+   $g2=4; 
+   $g3=5;
+   ?>
+   <h3>Your table should look like the one below</h3>
+   <?php
+   echo "
+   <table>
+   <tr>
+   <th>S.N</th><th>Name</th><th>Grade</th>
+   </tr>
+   <tr>
+   <td>1</td><td>pekka</td><td>$g1</td><br>
+   </tr>
+   <tr>
+   <td>2</td><td>Johanna</td><td>$g2</td><br>
+   </tr>
+   <tr>
+   <td>3</td><td>John</td><td>$g3</td><br>
+   </tr>
+   </table>
+   ";
+   ?>
+ <img src="docker.png">
 </body>
 </html>
