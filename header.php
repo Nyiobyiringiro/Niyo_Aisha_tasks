8a841eafa35ab531b23d4f5261ab07e6ac2c1821
Nyiobyiringiro <114920983+Nyiobyiringiro@users.noreply.github.com>
2023-02-09 19:42:06 +0200

    submit array

--- header.php
+++ header.php
@@ -107,7 +107,7 @@ background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32
               <a class="nav-link" href="controlflow.php">Control flow</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about.html">Exercise4</a>
+              <a class="nav-link" href="array.php">Arrays</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="fi.html">Exercise5</a>
